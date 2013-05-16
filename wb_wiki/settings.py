117ceea9a08d907ab59d4223a3a868a807f852a8
crazyzubr <hjcnbckfd@gmail.com>
2013-05-16 23:45:31 +0800

    wiki disallow anonymouse users

--- wb_wiki/settings.py
+++ wb_wiki/settings.py
@@ -145,6 +145,8 @@ INSTALLED_APPS = (
     'wiki.plugins.macros',
 )
 
+WIKI_ANONYMOUS = False
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
