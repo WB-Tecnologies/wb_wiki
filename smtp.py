aeeba7e176fdb4a6335fe58961538b1cbb892595
lionasp <lion.programmer@gmail.com>
2013-06-10 11:34:21 +0300

    smtp write log

--- smtp.py
+++ smtp.py
@@ -4,7 +4,8 @@ inbox = Inbox()
 
 @inbox.collate
 def handle(to, sender, subject, body):
-    print "work %s %s" % (to, sender)
+    log_file = open(r'smtp.log', 'w')
+    log_file.write("work %s %s" % (to, sender))
 
 # Bind directly.
 inbox.serve(address='0.0.0.0', port=4467)
