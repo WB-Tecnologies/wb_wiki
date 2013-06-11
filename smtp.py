9b7090518bbe4f496ae720c8a8c0c693b90fc402
lionasp <lion.programmer@gmail.com>
2013-06-11 12:39:51 +0300

    update smtp

--- smtp.py
+++ smtp.py
@@ -3,9 +3,13 @@ from inbox import Inbox
 inbox = Inbox()
 
 @inbox.collate
-def handle(to, sender, subject, body):
+def handle(to, sender, body):
+    text = body[body.find("\n", body.find('Content-Type: text/html;')):]
+    subject_pos = body.find('Subject')
+    subject = body[body.find(" ", subject_pos)+1:body.find("\n", subject_pos)]
+
     log_file = open(r'smtp.log', 'w')
-    log_file.write("work %s %s" % (to, sender))
+    log_file.write("%s\n%s\n%s\n%s\n" % (to, sender, subject, text))
 
 # Bind directly.
 inbox.serve(address='0.0.0.0', port=4467)
