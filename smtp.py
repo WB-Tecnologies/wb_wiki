df6455c592a9734398747e9e55144541d2b37f25
lionasp <lion.programmer@gmail.com>
2013-06-13 09:29:27 +0300

    test curl

--- smtp.py
+++ smtp.py
@@ -1,4 +1,6 @@
 from inbox import Inbox
+import os
+import json
 
 inbox = Inbox()
 
@@ -12,4 +14,29 @@ def handle(to, sender, body):
     log_file.write("%s\n%s\n%s\n%s\n" % (to[0], sender, subject, text))
 
 # Bind directly.
-inbox.serve(address='0.0.0.0', port=4467)
+#inbox.serve(address='0.0.0.0', port=4467)
+
+
+class Basecamp(object):
+
+    def __init__(self, login, password, project_id):
+        self.base_url = 'https://basecamp.com/1810831/api/v1/'
+        self.login = login
+        self.password = password
+        self.project_id = project_id
+
+    def _request(self, command):
+        text = ''
+        request_url = "curl -u %s:%s %sprojects/%s/%s.json" % (self.login, self.password, self.base_url, self.project_id, command)
+        print request_url
+        result = os.popen(request_url)
+        for line in result.readlines():
+            text += line
+        result.close()
+        print text
+        return json.loads(text)
+
+
+camp = Basecamp('lionasp', 'l80662', '3015357-helpdesk')
+print camp._request('todolists')
+
